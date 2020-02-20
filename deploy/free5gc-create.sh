75170baa8048c0c9f3c499fda73640cd2155c46a
tcc <tschchin@gmail.com>
2020-02-20 17:20:10 +0800

    add svc to export port of mongo, amf, upf

--- deploy/free5gc-create.sh
+++ deploy/free5gc-create.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 sudo kubectl apply -f free5gc-configmap
-sleep 2s
+sleep 3s
+sudo kubectl apply -f mongo.yaml
+sleep 10s
 sudo kubectl apply -f upf1.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f upf2.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f upf3.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f nrf.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f amf.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f smf.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f udr.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f pcf.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f udm.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f nssf.yaml
-sleep 2s
+sleep 3s
 sudo kubectl apply -f ausf.yaml
--- deploy/free5gc-delete.sh
+++ deploy/free5gc-delete.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo kubectl delete deployment free5gc-mongodb
 sudo kubectl delete deployment free5gc-nrf-deployment
 sudo kubectl delete deployment free5gc-amf-deployment
 sudo kubectl delete deployment free5gc-smf-deployment
@@ -11,3 +12,10 @@ sudo kubectl delete deployment free5gc-ausf-deployment
 sudo kubectl delete deployment free5gc-upf1-deployment
 sudo kubectl delete deployment free5gc-upf2-deployment
 sudo kubectl delete deployment free5gc-upf3-deployment
+
+
+sudo kubectl delete service mongodb-svc
+sudo kubectl delete service amf-svc
+sudo kubectl delete service upf1-svc
+sudo kubectl delete service upf2-svc
+sudo kubectl delete service upf3-svc
